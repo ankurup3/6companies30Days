dad0c61ba06abd0c24ce5a856fa8602f3e5f5b34
Ankur Upadhyay <73516124+ankurup3@users.noreply.github.com>
2022-01-02 22:49:42 +0530

    Question 12

---
+++ GoldmanSachs/q12.cpp
@@ -0,0 +1,30 @@
+//link to the question: https://practice.geeksforgeeks.org/problems/squares-in-nn-chessboard1801/1#
+// { Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution {
+  public:
+    long long squaresInChessBoard(long long n) {
+        long long int res = n*(n+1)*(2*n +1)/6;
+        return res;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.squaresInChessBoard(N) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
