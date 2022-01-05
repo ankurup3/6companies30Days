8b1d64b42601f533bfa7ada680ba6ecd298999a5
Ankur Upadhyay <73516124+ankurup3@users.noreply.github.com>
2022-01-05 14:56:48 +0530

    Question 10

---
+++ GoldmanSachs/q10.cpp
@@ -0,0 +1,50 @@
+//link to the question:
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to return k largest elements from an array.
+    vector<int> kLargest(int arr[], int n, int k)
+    {
+        vector<int> ans;
+        priority_queue<int> p(arr, arr+n);
+        while(k!=0) {
+            ans.push_back(p.top());
+            p.pop();
+            k--;
+        }
+        return ans;
+        // code here
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n, k;
+        cin >> n >> k;
+        
+        int arr[n];
+        for(int i = 0; i < n;i++)
+            cin>>arr[i];
+        Solution ob;
+        vector<int> result = ob.kLargest(arr, n, k);
+        for (int i = 0; i < result.size(); ++i)
+            cout<<result[i]<<" ";
+        cout << endl;
+        
+    }
+    return 0;
+}
+  // } Driver Code Ends 
